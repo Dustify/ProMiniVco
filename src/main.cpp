4d66b5b185a613c86c308572a8ff1ef3541ba05d
Adrian Johnson <aj@dustify.com>
2019-04-13 23:06:56 +0100

    basic sawtooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 #include <Arduino.h>
 
-#define INTERVAL 250
+#define PIN_TUNING A5
+#define COUNT_PHASE 2000
+
+static uint16_t position;
+static uint8_t wavetable[COUNT_PHASE];
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_TUNING, INPUT);
+
+  DDRD = B11111111;
+
+  for (uint16_t i = 0; i < COUNT_PHASE; i++)
+  {
+    wavetable[i] = ((double)i / (double)COUNT_PHASE) * (double)255;
+  }
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(INTERVAL);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(INTERVAL);
+  uint16_t value = (1023 - analogRead(PIN_TUNING)) / 2;
+  position += value;
+
+  int16_t offset = position - COUNT_PHASE;
+
+  if (offset > 0) {
+    position = offset;
+  }
+
+  PORTD = wavetable[position];
 }
