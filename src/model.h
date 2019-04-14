67d700c72b43d46ba671cfe7862cab0eead0a9bd
Adrian Johnson <aj@dustify.com>
2019-04-14 19:13:30 +0100

    #6 model comments

--- src/model.h
+++ src/model.h
@@ -1,3 +1,5 @@
+// shared objects
+
 #ifndef MODEL_H
 
 #define MODEL_H
@@ -7,11 +9,17 @@
 class Model
 {
   public:
+    // defines whether or not wavetable generation is taking place
     static bool busy;
+
+    // defines current position in wavetable
     volatile static uint16_t position;
+
+    // defines wavetable
     static uint8_t wavetable[COUNT_PHASE];
 };
 
+// initialise
 bool Model::busy;
 volatile uint16_t Model::position;
 uint8_t Model::wavetable[COUNT_PHASE];
