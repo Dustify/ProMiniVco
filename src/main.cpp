ef8d8c96176d0e7e8512b42e0e77e4824dfc930f
Adrian Johnson <aj@dustify.com>
2019-04-14 19:11:48 +0100

    #6 main comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// main entrypoint
+
 #include "config.h"
 #include "wavetable.h"
 #include "output.h"
@@ -6,23 +8,38 @@
 
 void setup()
 {
+  // set pinmodes
+  // output LED
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  // tuning pin
   pinMode(PIN_TUNING, INPUT);
+
+  // CV pin
   pinMode(PIN_CV, INPUT);
+
+  // wavetable cycle button
   pinMode(PIN_WAVE, INPUT_PULLUP);
 
+  // set DAC port to all output
   DDRD = B11111111;
 
+  // initialise wavetable - default sine
   WaveTable::generate_sine();
 
+  // setup interrupt timer
   Timer1.initialize(1e6 / SAMPLE_RATE);
   Timer1.attachInterrupt(Output::Tick);
 }
 
 void loop()
 {
+  // check if wavetable cycle button has been pressed
   bool waveChangeState = digitalRead(PIN_WAVE) == LOW;
+
+  // ...illuminate LED if pressed
   digitalWrite(LED_BUILTIN, waveChangeState ? HIGH : LOW);
 
+  // pass button state to handler
   WaveTable::handleWaveChange(waveChangeState);
 }
