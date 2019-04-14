375c2fdf9e6ea131da7de83d741d93521f40115d
Adrian Johnson <aj@dustify.com>
2019-04-14 19:16:13 +0100

    #6 output comments

--- src/output.h
+++ src/output.h
@@ -1,3 +1,5 @@
+// handles output of wavetable values to DAC
+
 #ifndef OUTPUT_H
 
 #define OUTPUT_H
@@ -7,27 +9,29 @@
 class Output
 {
   public:
+    // executes on each 'tick' - based on samplerate
     static void Tick()
     {
+        // if wavetable generation is taking place then do nothing
         if (Model::busy)
         {
             return;
         }
 
+        // read values from analog pins and divide by factor
         uint16_t value = (analogRead(PIN_TUNING) + analogRead(PIN_CV)) / FACTOR;
 
-        // value = 1023.0 - value; // reverse pot direction
-        // value /= 1023.0;
-        // value = pow(value, 2.0); // 'expo' style smoothing
-        // value *= 200.0;          // frequency limiting
-
+        // increment position based on value
         Model::position += value;
 
+        // if position has exceeded number of phases
         if (Model::position >= COUNT_PHASE)
         {
+            // ...wrap back around
             Model::position = Model::position - COUNT_PHASE;
         }
 
+        // send value to DAC
         PORTD = Model::wavetable[Model::position];
     }
 };
